8f88d31296a6b9fda03ed9dd92e6b8f247e754df
fabienchevalier <97484369+fabienchevalier@users.noreply.github.com>
2024-03-04 14:43:06 +0100

    img

--- TPs/tp1/main.go
+++ TPs/tp1/main.go
@@ -1,15 +1,53 @@
 package main
 
 import (
+	"crypto/sha256"
+	"flag"
 	"fmt"
-	"log"
-	"os/exec"
+	"os"
+)
+
+var (
+	// Image paths
+	image1Path = flag.String("image1", "", "Path to the first image")
+	image2Path = flag.String("image2", "", "Path to the second image")
 )
 
 func main() {
-	out, err := exec.Command("date").Output()
-	if err != nil {
-		log.Fatal(err)
+	flag.Parse()
+
+	// Handle empty flag
+	if *image1Path == "" || *image2Path == "" {
+		fmt.Println("Please provide both file paths using the flags -file1 and -file2")
+		return
 	}
-	fmt.Printf("The date is: %s\n", out)
+
+	// Generate img hash
+	hash1, err := hashFile(*image1Path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	hash2, err := hashFile(*image2Path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if hash1 == hash2 {
+		fmt.Println("The images are the same.")
+	} else {
+		fmt.Println("The images are different.")
+	}
+}
+
+func hashFile(filename string) (string, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+
+	hash := sha256.Sum256(data)
+	return fmt.Sprintf("%x", hash), nil
 }
